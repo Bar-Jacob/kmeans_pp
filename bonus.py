ce463c9097120076227a3a6b08f114b83f8b190d
gendelev1 <73215998+gendelev1@users.noreply.github.com>
2021-06-13 16:52:08 +0300

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,28 @@
+from matplotlib.text import Annotation
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import numpy as np
+
+
+data_set = datasets.load_iris()
+data_frame = data_set.data
+
+inertia_list = []
+for k in range(1, 10):
+    kmeans = KMeans(n_clusters=k, init='k-means++',
+                    random_state=0)
+    kmeans.fit(data_frame)
+    inertia_list.append(kmeans.inertia_)
+
+inertia_plot = plt.figure()
+plt.plot(range(1, 10), inertia_list)
+plt.title('Elbow method for selection of optimal "k" clusters')
+plt.xlabel('K')
+plt.ylabel('Average Dispersion')
+
+plt.annotate("Elbow point", (2.3, inertia_list[1]), xytext=(
+    3, 170), arrowprops=dict(facecolor='pink', shrink=0.05, linestyle="--"))
+plt.scatter(2, inertia_list[1], s=500,
+            facecolors='none', edgecolors='k', linestyle="--")
+plt.savefig("elbow.png")
